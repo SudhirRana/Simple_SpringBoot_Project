fac0ac250c870cfccac02fa48b6724e68fef464b
SudhirRana <sudhirrana500@gmail.com>
2022-05-19 23:33:03 +0530

    Fist Commit

---
+++ src/main/java/com/Simple_SpringBoot_Project/App.java
@@ -0,0 +1,14 @@
+package com.Simple_SpringBoot_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("You are CCTV survielance");
+    }
+}
